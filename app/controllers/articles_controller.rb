bbb8c2b52664ba5cdcb5c1c4fecd6356cb398e8b
Quentin Johnson <qjohnson1@gmail.com>
2020-07-07 22:21:03 -0500

    deploy latest updates to blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,56 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def index
         @articles = Article.all
     end
 
     def show
         #byebug
-        @article = Article.find(params[:id])
     end
 
     def new
+        @article = Article.new
     end
 
     def create
-        title = params[:article][:title]
-        description = params[:article][:description]
-        Article.create(title: title, description: description)
-        redirect_to articles_path
+        @article = Article.new(article_params)
+        #render plain: params[:article]
+        if @article.save
+            #redirect_to article_path(@article)
+            flash[:notice] = "Article successfully saved."
+            redirect_to @article
+        else
+            render 'new'    
+        end
+    end
+
+    def edit
     end
 
     def update
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+
+    def set_article
+        @article = Article.find(params[:id])
     end
 
 end
